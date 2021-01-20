d79ef5253fd3e236194b8e6094ee26fb6d7cc0c2
capodisseny <36953932+capodisseny@users.noreply.github.com>
2021-01-20 10:02:19 +0100

    Create airtable-js-api-calls.js

---
+++ airtable-js-api-calls.js
@@ -0,0 +1,151 @@
+
+let  apiCall = function(baseId, tableId, params = {}, method = "GET"){
+
+    ////////console.log('asdqasda')
+
+
+    let url = new URL(`https://api.airtable.com/v0/${baseId}/${tableId}`)
+
+
+     let request = {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer keyZS9YvfpGLVeQD0',
+           // 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH'
+        }
+        }
+
+    
+       if(typeof params == "string"){
+
+           this.url = url+"/"+params
+
+        }else if(typeof params == "object"){
+
+
+            switch (method){
+                case "GET":
+
+                    let urlFields = []
+                    if(params.fields){
+                        urlFields = [...params.fields]
+                    }
+
+                    delete params.fields
+
+                    url.search = new URLSearchParams({
+
+                        ...params
+                        
+                    })
+
+
+
+                    
+
+                    //convert fields to 
+                    let fetchUrl = url.href
+
+                    ////////console.log(urlFields)
+
+                    urlFields.forEach(field =>{
+                    field =  field.replace(" ", "+")
+                        fetchUrl += "&fields%5B%5D=" + field
+
+                    
+                    })
+
+                    this.url = fetchUrl
+
+
+                break;
+                default:
+
+                //default url
+                this.url =  url.href
+
+                console.log(this, this.url)
+
+
+                request["body"] = JSON.stringify(params)
+
+            
+                //data as params
+
+
+                break;
+            }
+
+            
+
+        }else{
+
+            return "failed"
+
+        }
+    
+
+
+
+    
+
+
+ console.log(request, this.url)
+
+    
+    
+    this.data = async function(){
+        let response = await fetch(this.url, request )
+
+    let data = await response.json()
+    return data
+    }
+
+    return this
+
+}
+
+
+
+    const apiGet = async function (base, table, params = {} ){
+
+        let call = new apiCall(base, table, params , "GET")
+
+         let data = await call.data()
+
+         return data
+
+    }
+    const apiUpdate = async function (base, table, params = {} ){
+
+        let call = new apiCall(base, table, params , "PATCH")
+
+         let data = await call.data()
+
+         return data
+
+    }
+
+    const apiCreate = async function (base, table, params = {} ){
+
+        let call = new apiCall(base, table, params , "POST")
+
+            let data = await call.data()
+
+            return data
+
+    }
+
+    const apiDelete = async function (base, table, params = {} ){
+
+     
+
+
+        let call = new apiCall(base, table, params , "DELETE")
+
+            let data = await call.data()
+
+            return data
+
+    }
